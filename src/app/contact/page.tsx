0cf9f023fcb3b15295cc857a907aaf28e3e660cc
asvayuktechnology <asvayuktech@gmail.com>
2026-06-10 11:41:50 +0530

    fix image issue

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,13 +4,18 @@ import { BASE_URL } from "@/lib/const";
 import { getContactInfo } from "@/services/contactService";
 
 export default async function Contact() {
-  const response = await getContactInfo();
+  let bannerImage = "/assets/img/breadcrumb-bg3.jpg";
 
-  const contactData = response?.data?.[0];
+  try {
+    const response = await getContactInfo();
+    const contactData = response?.data?.[0];
 
-  const bannerImage = contactData?.bannerImage
-    ? `${BASE_URL}/${contactData?.bannerImage}`
-    : "/assets/img/breadcrumb-bg2.jpg";
+    if (contactData?.bannerImage) {
+      bannerImage = `${BASE_URL}/${contactData.bannerImage}`;
+    }
+  } catch (error) {
+    console.error("Failed to fetch contact banner:", error);
+  }
 
   return (
     <>
